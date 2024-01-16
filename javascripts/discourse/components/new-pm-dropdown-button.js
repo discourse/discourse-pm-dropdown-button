b550e876fdd3ba29757506eba7864b6bab2fc280
awesomerobot <kris.aubuchon@discourse.org>
2024-01-16 12:12:35 -0500

    DEV: remove legacy option, use service, pin to previous version

--- javascripts/discourse/components/new-pm-dropdown-button.js
+++ javascripts/discourse/components/new-pm-dropdown-button.js
@@ -1,24 +1,14 @@
 import Component from "@glimmer/component";
-import Composer from "discourse/models/composer";
 import { action } from "@ember/object";
 import { inject as service } from "@ember/service";
-import { getOwner } from "discourse-common/lib/get-owner";
 
 export default class NewPMDropdownButton extends Component {
   @service currentUser;
-
-  #createPM() {
-    const composer = getOwner(this).lookup("controller:composer");
-    composer.open({
-      action: Composer.PRIVATE_MESSAGE,
-      draftKey: Composer.NEW_PRIVATE_MESSAGE_KEY,
-      archetypeId: "private_message",
-    });
-  }
+  @service composer;
 
   @action
   openComposerCloseMenu() {
-    this.#createPM();
     this.args.closeUserMenu();
+    this.composer.openNewMessage({});
   }
 }
